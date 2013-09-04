1ebc81c03f4ddfe56a118e23bef1631c0b6c9aee
Jason Wolfe <jason@w01fe.com>
2013-09-03 18:31:31 -0700

    Organize project and add changelog

--- project.clj
+++ project.clj
@@ -1,22 +1,20 @@
 (defproject prismatic/schema "0.0.4-SNAPSHOT"
-
+  :description "Clojure(Script) library for declarative data description and validation"
   :url "http://github.com/prismatic/schema"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :scm {:url "git@github.com:prismatic/schema.git"}
+  :dependencies [[prismatic/cljs-test "0.0.6"]
+                 [potemkin "0.3.2"]]
 
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                                  [com.keminglabs/cljx "0.3.0"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl
+                                                     cljx.repl-middleware/wrap-cljx]}}}
 
   :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
 
   :plugins [[lein-cljsbuild "0.3.2"] [com.keminglabs/cljx "0.3.0"]]
-
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [prismatic/cljs-test "0.0.6"]
-                 [potemkin "0.3.2"]]
-
-  :source-paths ["target/generated/src/clj" "src/clj"]
-  :test-paths ["target/generated/test/clj" "test/clj"]
 
   :lein-release {:deploy-via :shell
                  :shell ["lein" "deploy" "clojars"]}
@@ -24,18 +22,21 @@
   :cljx {:builds [{:source-paths ["src/cljx"]
                    :output-path "target/generated/src/clj"
                    :rules :clj}
-
                   {:source-paths ["src/cljx"]
                    :output-path "target/generated/src/cljs"
                    :rules :cljs}
-
                   {:source-paths ["test/cljx"]
                    :output-path "target/generated/test/clj"
                    :rules :clj}
-
                   {:source-paths ["test/cljx"]
                    :output-path "target/generated/test/cljs"
                    :rules :cljs}]}
+
+  :prep-tasks ["cljx" "javac" "compile"]
+
+  :source-paths ["target/generated/src/clj" "src/clj"]
+
+  :test-paths ["target/generated/test/clj" "test/clj"]
 
   :cljsbuild {:builds
               {:dev {:source-paths ["src/clj" "target/generated/src/cljs"]
@@ -46,10 +47,4 @@
                                       "target/generated/src/cljs" "target/generated/test/cljs"]
                       :compiler {:output-to "target/unit-test.js"
                                  :optimizations :whitespace
-                                 :pretty-print true}}}}
-
-  :prep-tasks ["cljx" "javac" "compile"]
-
-  :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.3.0"]]
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl
-                                                     cljx.repl-middleware/wrap-cljx]}}})
+                                 :pretty-print true}}}})
