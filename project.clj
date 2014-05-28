1d96ccacde23535d9285939955370fcb4a25ddc1
loganlinn <logan.linn@gmail.com>
2014-05-28 16:22:42 -0700

    lein-release plugin: bumped version from 0.2.3 to 0.2.4-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/schema "0.2.3"
+(defproject prismatic/schema "0.2.4-SNAPSHOT"
   :description "Clojure(Script) library for declarative data description and validation"
   :url "http://github.com/prismatic/schema"
   :license {:name "Eclipse Public License"
