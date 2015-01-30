df92eb4c70a5758a5118c3d121dd6aac88c9b336
Jason Wolfe <jason@w01fe.com>
2015-01-29 22:26:38 -0800

    lein-release plugin: preparing 0.3.5 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/schema "0.3.5-SNAPSHOT"
+(defproject prismatic/schema "0.3.5"
   :description "Clojure(Script) library for declarative data description and validation"
   :url "http://github.com/prismatic/schema"
   :license {:name "Eclipse Public License"
