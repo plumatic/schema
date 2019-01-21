58138fd04a3e222c9c5c77161b9866609bdf0bcb
Jason Wolfe <jason@w01fe.com>
2019-01-20 21:36:57 -0800

    lein-release plugin: bumped version from 1.1.10 to 1.1.11-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/schema "1.1.10"
+(defproject prismatic/schema "1.1.11-SNAPSHOT"
   :description "Clojure(Script) library for declarative data description and validation"
   :url "http://github.com/plumatic/schema"
   :license {:name "Eclipse Public License"
