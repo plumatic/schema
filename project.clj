decd5dd0fe252df9385a76fb492c7cfd6df47e29
Gary Fredericks <fredericksgary@gmail.com>
2019-08-10 21:11:04 -0500

    lein-release plugin: preparing 1.1.12 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/schema "1.1.12-SNAPSHOT"
+(defproject prismatic/schema "1.1.12"
   :description "Clojure(Script) library for declarative data description and validation"
   :url "http://github.com/plumatic/schema"
   :license {:name "Eclipse Public License"
