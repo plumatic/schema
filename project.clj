64e55dc57631e84adae4be5191757ef8639d74b7
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-07-28 15:22:11 -0400

    lein-release plugin: preparing 1.3.4 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/schema "1.3.4-SNAPSHOT"
+(defproject prismatic/schema "1.3.4"
   :description "Clojure(Script) library for declarative data description and validation"
   :url "http://github.com/plumatic/schema"
   :license {:name "Eclipse Public License"
@@ -17,13 +17,14 @@
                              [lein-release/lein-release "1.0.4"]
                              [lein-doo "0.1.10"]
                              [lein-pprint "1.3.2"]
-                             [lein-shell "0.5.0"]
                              [jonase/eastwood "1.2.3"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"] [org.clojure/clojurescript "1.10.520"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.3"] [org.clojure/clojurescript "1.10.879"]]}
              :1.11 {:dependencies [[org.clojure/clojure "1.11.1"] [org.clojure/clojurescript "1.11.4"]]}
              :1.12 {:dependencies [[org.clojure/clojure "1.12.0-master-SNAPSHOT"] [org.clojure/clojurescript "1.11.4"]]
                     :repositories [["sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public"}]]}}
+
+  :plugins [[lein-shell "0.5.0"]]
 
   :aliases {"all" ["with-profile" "+dev:+1.9:+1.10:+1.11:+1.12"]
             "deploy" ["do" "clean," "deploy" "clojars"]
@@ -63,12 +64,13 @@
 
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag"]
-                  ["deploy"]
+                  ;["vcs" "commit"]
+                  ;["vcs" "tag"]
+                  ;["deploy"]
                   ["shell" "./bin/push_docs_for_current_commit.sh"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]
-                  ["vcs" "push"]]
+                  ;["change" "version" "leiningen.release/bump-version"]
+                  ;["vcs" "commit"]
+                  ;["vcs" "push"]
+                  ]
 
   :signing {:gpg-key "66E0BF75"})
