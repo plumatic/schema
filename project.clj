2f4da86bc2403ecbf036654ffc4a56cb2978cdbc
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2022-07-28 14:53:43 -0400

    lein-release plugin: preparing 1.3.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/schema "1.3.1-SNAPSHOT"
+(defproject prismatic/schema "1.3.1"
   :description "Clojure(Script) library for declarative data description and validation"
   :url "http://github.com/plumatic/schema"
   :license {:name "Eclipse Public License"
