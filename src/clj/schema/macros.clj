905b26db7e198efec0d6f425e674098992d98a83
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-08-16 19:18:04 -0400

    explain __methodImplCache

--- src/clj/schema/macros.clj
+++ src/clj/schema/macros.clj
@@ -417,7 +417,12 @@
                                    [InnerMth
                                     (named (=> Any OuterMth InnerMth)
                                            'sync!)])]
-  (let [;; propagate method cache to inner method
+  (let [;; propagate method cache to inner method.
+        ;; explanation: all functions in Clojure have special support for protocol methods
+        ;; via the __methodImplCache field: https://github.com/clojure/clojure/search?q=methodimplcache&type=.
+        ;; this mutable field is used inside each protocol method's implementation via (fn this [..] (.__methodImplCache this))
+        ;; and also mutated from the "outside" via (set! .__methodImplCache protocol-method).
+        ;; since we wrap protocol methods, we need to preserve the these two features (settable from outside, readable from inside).
         sync! (fn [^clojure.lang.AFunction outer-mth
                    ^clojure.lang.AFunction inner-mth]
                 (when-not (identical? (.__methodImplCache outer-mth)
